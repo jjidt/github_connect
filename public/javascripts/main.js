888861bd03d72551bf314bd667aeb83d9b8d623d
JJ Idt <johnidt@gmail.com>
2014-11-09 12:25:30 -0800

    change bower vendor destination, wrap requireJS main module

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,19 +1,13 @@
-function(require) {
+(function () {
 	require.config({
-    baseUrl: 'javascripts/lib',
+    baseUrl: 'vendor',
     paths: {
-        // the left side is the module ID,
-        // the right side is the path to
-        // the jQuery file, relative to baseUrl.
-        // Also, the path should NOT include
-        // the '.js' file extension. This example
-        // is using jQuery 1.9.0 located at
-        // js/lib/jquery-1.9.0.js, relative to
-        // the HTML page.
-        jquery: 'jquery-1.11.1.min'
+        jquery: 'jquery/dist/jquery.min'
     	}
 	});
 
-
-
-}
+	require(['jquery'], function ($) {
+		console.log($);
+	});
+	
+}())
