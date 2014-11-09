cfd6195fccc1d67a4cb6f10156043d3a67641bf5
JJ Idt <johnidt@gmail.com>
2014-11-09 13:23:16 -0800

    add parsing of JSON socket data

--- public/javascripts/frontSocket.js
+++ public/javascripts/frontSocket.js
@@ -1,7 +1,13 @@
-define(['socketio'], function (io) {
-	var socket = io.connect('http://localhost');
+define(['socketio', 'jquery'], function (io, $) {
+	var socket = io.connect('http://localhost'),
+		contentContainer;
 	socket.on('news', function (data) {
-		
+		contentContainer = $('.content');
+		data = JSON.stringify(data);
+		window.data = JSON.parse(data);
+		// data.forEach(function(result) {
+		// 	contentContainer.append('<img src=' + result.avatar_url + '/>')
+		// });
 		socket.emit('my other event', { data: 'passed from the client' });
 	});
 });
