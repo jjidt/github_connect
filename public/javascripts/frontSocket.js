20b5e715e69767d26c66661daec4b9d3d5c07b4e
JJ Idt <johnidt@gmail.com>
2014-11-10 14:29:43 -0800

    change title, add login name on display page

--- public/javascripts/frontSocket.js
+++ public/javascripts/frontSocket.js
@@ -6,7 +6,7 @@ define(['socketio', 'jquery'], function (io, $) {
 		window.data = data;
 		data.items.forEach(function(result) {
 			console.log(result);
-			contentContainer.append('<p><img width="50px" src=' + result.avatar_url + '/></p>')
+			contentContainer.append('<p><img width="50px" src=' + result.avatar_url + '/>' + result.login + '</p>')
 		});
 		socket.emit('my other event', { data: 'passed from the client' });
 	});
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var express = require('express'),
 
 /* GET home page. */
 router.get('/', function(req, res) {
- 	res.render('index', { title: 'Express' });
+ 	res.render('index', { title: 'Github Portland' });
 });
 
 router.get('/portland', function(req, res) {
