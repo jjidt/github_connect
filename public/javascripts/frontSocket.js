060682defcca050dfe5dfd1a50a7343f14a4c2a9
JJ Idt <johnidt@gmail.com>
2014-11-10 17:33:24 -0800

    add alternate options path for getting user info

--- public/javascripts/frontSocket.js
+++ public/javascripts/frontSocket.js
@@ -1,17 +1,19 @@
 define(['socketio', 'jquery'], function (io, $) {
 	var socket = io.connect('http://localhost'),
-		contentContainer;
+		contentContainer,
+		url;
+
 	socket.on('news', function (data) {
 		contentContainer = $('.content');
 		window.data = data;
 		data.items.forEach(function(result) {
 		console.log(result);
-			contentContainer.append('<p><img class="user-image" width="50px" src=' + result.avatar_url + ' id=' + result.url + '/>' + result.login + '</p>')
+			contentContainer.append('<p><img class="user-image" width="50px" src=' + result.avatar_url + ' id=' + result.login + '/>' + result.login + '</p>')
 		});
 		$('.user-image').on('click', function(){
-			var url = $(this).attr('id');
+			url = $(this).attr('id');
 			console.log(url);
-			socket.emit('userSelect', "Blah Blah Blah");
+			socket.emit('userSelect', url);
 		});
 	});
 });
